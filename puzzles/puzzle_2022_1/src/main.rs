6beaae66fa24c8eacab6e4e867a33685e425ab66
73nko <apramos89@gmail>
2022-12-01 10:22:15 +0100

    Fix tests of day 1

--- puzzles/puzzle_2022_1/src/main.rs
+++ puzzles/puzzle_2022_1/src/main.rs
@@ -1,68 +1,61 @@
 use anyhow::{Ok, Result};
 
-type ElvesCalories = Vec<Vec<usize>>;
+type ElfCalories = Vec<usize>;
 
-fn part1(input: &ElvesCalories) -> Result<usize> {
-    let result = input.iter().map(|elf| elf.iter().sum()).max();
-
-    println!("{:?}", result);
-    Ok(result.unwrap())
+fn solve(input: &ElfCalories, elves: usize) -> Result<usize> {
+    Ok(input.iter().take(elves).sum())
 }
 
-fn part2(input: &ElvesCalories) -> Result<usize> {
-    let mut result: Vec<usize> = input.iter().map(|elf| elf.iter().sum()).collect();
-
-    result.sort();
-
-    let result = result.iter().rev().take(3).sum();
-
-    Ok(result)
-}
-
-fn prepare_input(input: &str) -> ElvesCalories {
-    input
+fn prepare_input(input: &str) -> ElfCalories {
+    let mut formatted_input: ElfCalories = input
         .split("\n\n")
         .map(|elf_cal| {
             elf_cal
                 .lines()
-                .map(|cals| cals.parse::<usize>().unwrap())
+                .map(|cals| cals.trim().parse::<usize>().unwrap())
                 .collect()
         })
-        .collect()
+        .map(|elf: Vec<usize>| elf.iter().sum())
+        .collect();
+
+    formatted_input.sort();
+    formatted_input.reverse();
+
+    formatted_input
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     const INPUT: &str = r#"1000
-2000
-3000
+    2000
+    3000
 
-4000
+    4000
 
-5000
-6000
+    5000
+    6000
 
-7000
-8000
-9000
+    7000
+    8000
+    9000
 
-10000"#;
+    10000"#;
     #[test]
     fn test_part1() {
         let input = prepare_input(INPUT);
-        assert_eq!(part1(&input).unwrap(), 24000);
+        assert_eq!(solve(&input, 1).unwrap(), 24000);
     }
     #[test]
     fn test_part2() {
         let input = prepare_input(INPUT);
-        assert_eq!(part2(&input).unwrap(), 45000);
+        assert_eq!(solve(&input, 3).unwrap(), 45000);
     }
 }
 
 fn main() -> Result<()> {
-    let input = include_str!("../input.txt");
-    println!("PART 1: {:?}", part1(&prepare_input(input))?);
-    println!("PART 2: {:?}", part2(&prepare_input(input))?);
+    let input = prepare_input(include_str!("../input.txt"));
+    println!("PART 1: {:?}", solve(&input, 1)?);
+    println!("PART 2: {:?}", solve(&input, 3)?);
     Ok(())
 }
